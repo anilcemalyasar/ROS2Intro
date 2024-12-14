9a950c732c711686358997066f67dcdae073eb1e
anilcemalyasar <anlysr2002@gmail.com>
2024-12-14 12:27:22 +0300

    creating ros2 workspace, c++ and python package, nodes with oop in both python and cpp

---
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "rclcpp/rclcpp.hpp"
+
+// Creating C++ Node with OOP
+class MyNode: public rclcpp::Node
+{
+    public:
+        MyNode(): Node("cpp_test"), counter_(0)
+        {
+            RCLCPP_INFO(this->get_logger(), "Hello Cpp Node");
+        
+            timer_ = this->create_wall_timer(std::chrono::seconds(1),
+                                             std::bind(&MyNode::timerCallBack, this));
+        };
+    
+    private:
+
+    void timerCallBack()
+    {
+        counter_++;
+        RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_;
+};
+
+int main(int argc, char **argv){
+
+    // Initialize ROS2 communication
+    rclcpp::init(argc, argv);
+
+    // Object created into Shared pointer 
+    // auto node = std::make_shared<rclcpp::Node>("cpp_test");
+
+    // RCLCPP_INFO(node->get_logger(), "Hello CPP Node");
+
+    auto node = std::make_shared<MyNode>();
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+# Node is not an executable object byself
+
+# inheriting from Node class
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("py_test")
+        self.counter_ = 0
+        self.get_logger().info("Hello ROS2")
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info("Hello " + str(self.counter_))
+
+def main(args=None):
+    rclpy.init(args=args)  # starts ROS2 communication and other features
+
+    # node = Node("py_test")  # first node created
+    # node.get_logger().info("Hello ROS2") 
+    node = MyNode()
+
+    rclpy.spin(node)  # node will be spinning and program wont stop
+    rclpy.shutdown() # shutting down opened ros2 communication
+
+if __name__ == "__main__":
+    main()
