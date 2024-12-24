3ffd46230e36dbcda8eebc39a1da2389460c3e32
anilcemalyasar <anlysr2002@gmail.com>
2024-12-24 23:47:55 +0300

    reset counter service created using example_interfaces SetBool

--- src/my_py_pkg/my_py_pkg/number_subscriber.py
+++ src/my_py_pkg/my_py_pkg/number_subscriber.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from example_interfaces.srv import SetBool
 
 class NumberCounterNode(Node):
     
@@ -11,6 +12,9 @@ class NumberCounterNode(Node):
             Int64, "number_count", 10
         )
         self.subscriber_ = self.create_subscription(Int64, "number", self.callback_number, 10)
+        
+        self.reset_counter_service_ = self.create_service(
+            SetBool, "reset_counter", self.callback_reset_counter)
         self.get_logger().info("Number Counter has been started")
         
     def callback_number(self, msg):
@@ -19,6 +23,17 @@ class NumberCounterNode(Node):
         new_msg.data = self.counter_
         self.number_count_publisher_.publish(new_msg)
         # self.get_logger().info(str(self.counter_))
+        
+    def callback_reset_counter(self, request, response):
+        if request.data:
+            response.success = True
+            response.message = "Counter has been reset"
+            self.counter_ = 0
+        else:
+            response.success = False
+            response.message = "Counter has not been reset"
+            
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
