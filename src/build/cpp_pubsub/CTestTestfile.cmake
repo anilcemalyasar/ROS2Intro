f89916c5b37cd54a407a1ffdf3e1d71c2dba2b4e
anilcemalyasar <anlysr2002@gmail.com>
2024-12-27 20:52:31 +0300

    gitignore conflict resolved

---
+++ src/build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/acy/ros2_ws/src/cpp_pubsub
+# Build directory: /home/acy/ros2_ws/src/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
