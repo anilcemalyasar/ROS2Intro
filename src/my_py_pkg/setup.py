75fbfe546f585e91a83f009a8fe0d4044a121767
anilcemalyasar <anlysr2002@gmail.com>
2025-01-11 18:09:08 +0300

    creating battery as a client of set_led serice, based on battery state led panel is switched

--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -34,7 +34,8 @@ setup(
             "hw_status_subscriber = my_py_pkg.hw_status_subscriber:main",
             "webcam_image_publisher = my_py_pkg.webcam_image_publisher:main",
             "webcam_image_subscriber = my_py_pkg.webcam_image_subscriber:main",
-            "led_panel = my_py_pkg.led_panel:main"
+            "led_panel = my_py_pkg.led_panel:main",
+            "battery = my_py_pkg.battery:main"
         ],
     },
 )
